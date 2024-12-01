0ef8c0c7241a999249303d1b740d51eb9e0c4a90
AkramMahmoudi <140149262+AkramMahmoudi@users.noreply.github.com>
2024-12-01 12:44:57 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div className="parent">
+{/*         when remove sidebar for test mobile view main and header use 1fr to fill space of side bar */}
         <div className="sidebar">sidebar</div>
         <div className="header">header</div>
         <div className="main">main</div>
