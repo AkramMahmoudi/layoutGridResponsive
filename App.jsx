eb3a162a9737f03495d20014e2197e740fcd6afb
AkramMahmoudi <140149262+AkramMahmoudi@users.noreply.github.com>
2024-12-01 10:43:12 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="parent">
+        <div className="sidebar">sidebar</div>
+        <div className="header">header</div>
+        <div className="main">main</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
